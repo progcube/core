f4693c337c32c32482de68f70caa7be63aab957b
mleboeuf <ml@maximeleboeuf.com>
2017-09-06 00:11:26 -0400

    Initial static Progcube class

---
+++ Progcube.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Progcube.Core
+{
+    /// <summary>
+    /// Represents a static entry point to bind the Progcube framework to your application.
+    /// </summary>
+    public static class Progcube
+    {
+        /// Call this method in your application's Startup.ConfigureServices().
+        public static void Bind(IServiceCollection services)
+        {
+            // TODO: bind mediator here
+        }
+    }
+}
